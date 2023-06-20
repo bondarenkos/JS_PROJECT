716601332488600178c23aa8592d1caf423e5c6b
Oleksandr <sb08597@gmail.com>
2023-06-20 05:13:21 +0200

    4 commit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import requests
+import tkinter as tk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 class WeatherForecast:
@@ -35,29 +38,45 @@ class WeatherForecast:
         if response.status_code == 200:
             data = response.json()
             for hour in data["forecast"]['forecastday'][0]['hour']:
-                print(hour)
                 time = hour['time']
                 temp_c = hour['temp_c']
                 temp_f = hour['temp_f']
-                wind_mph = hour['condition']['wind_mph']
-                wind_kph = hour['condition']['mintemp_f']
-                pressure_mb = hour['condition']['totalprecip_mm']
-                pressure_in = hour['condition']['totalprecip_in']
-                precip_mm = hour['condition']['avgvis_km']
-                precip_in = hour['condition']['avgvis_miles']
-                humidity = hour['condition']['humidity']
-                feelslike_c = hour['condition']['feelslike_c']
-                feelslike_f = hour['condition']['feelslike_f']
-                vis_km = hour['condition']['vis_km']
-                vis_miles = hour['condition']['vis_miles']
+                wind_mph = hour['wind_mph']
+                wind_kph = hour['wind_kph']
+                pressure_mb = hour['pressure_mb']
+                pressure_in = hour['pressure_in']
+                precip_mm = hour['precip_mm']
+                precip_in = hour['precip_in']
+                humidity = hour['humidity']
+                feelslike_c = hour['feelslike_c']
+                feelslike_f = hour['feelslike_f']
+                vis_km = hour['vis_km']
+                vis_miles = hour['vis_miles']
                 weather[time] = (temp_c, temp_f, wind_kph, wind_mph, pressure_mb, pressure_in, precip_in,
                                  precip_mm, humidity, feelslike_c, feelslike_f, vis_km, vis_miles)
         return weather
+
+    def build_giu(self):
+        root = tk.Tk()
+        window_width = 800
+        window_height = 600
+        root.geometry(f"{window_width}x{window_height}")
+        root.title("Weather")
+
+        lable = tk.Label(root, text="Chcesz zaplanować urlop?", font=("Arial", 25, 'bold'))
+        lable.place(x=190, y=100)
+
+        button1 = tk.Button(root, text="Nie", font=("Arial", 20))
+        button1.place(x=210, y=250, width=150, height=50)
+
+        button2 = tk.Button(root, text="Tak", font=("Arial", 20))
+        button2.place(x=430, y=250, width=150, height=50)
+
+        root.mainloop()
 
 
 api_key = "112aff8f7d04491cb93204822231906"
 location = "Wroclaw"
 
 weather_forecast = WeatherForecast(api_key)
-# weather_forecast.get_forecast_month(location, date)
-weather_forecast.get_forecast_day(location)
+weather_forecast.build_giu()
