bdcbc7fe6408a3cecc73ff9f5e01ab1590c5e500
Oleksandr <sb08597@gmail.com>
2023-06-20 03:53:06 +0200

    2 commit

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
 import requests
 
-api_key = "112aff8f7d04491cb93204822231906"
-location = "London"
-date=["2023-09-22", "2023-09-23", "2023-09-24", "2023-09-25", "2023-09-26", "2023-09-27"]
 
-for d in range(1,30,1):
-    url = f"http://api.weatherapi.com/v1/future.json?key=112aff8f7d04491cb93204822231906&q=Wroclaw&dt=2023-09-{d}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        #print(f"2023-09-{d} {data['forecast']['forecastday'][0]['day']['maxtemp_c']} {data['forecast']['forecastday'][0]['day']['mintemp_c']}")
-        print(data['forecast']['forecastday'][0]['hour'])
-        for h in data['forecast']['forecastday'][0]['hour']:
-            print(h['time'], h['temp_c'])
-    else:
-        print("Ошибка при получении прогноза погоды.")
+class WeatherForecast:
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def get_forecast_month(self, location, date):
+        weather = {}
+        for d in date:
+            url = f"http://api.weatherapi.com/v1/future.json?key={self.api_key}&q={location}&dt={d}"
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                date = data['forecast']['forecastday'][0]['date']
+                maxtemp_c = data['forecast']['forecastday'][0]['day']['maxtemp_c']
+                maxtemp_f = data['forecast']['forecastday'][0]['day']['maxtemp_f']
+                mintemp_c = data['forecast']['forecastday'][0]['day']['mintemp_c']
+                mintemp_f = data['forecast']['forecastday'][0]['day']['mintemp_f']
+                maxwind_mph = data['forecast']['forecastday'][0]['day']['maxwind_mph']
+                maxwind_kph = data['forecast']['forecastday'][0]['day']['maxwind_kph']
+                totalprecip_mm = data['forecast']['forecastday'][0]['day']['totalprecip_mm']
+                totalprecip_in = data['forecast']['forecastday'][0]['day']['totalprecip_in']
+                avgvis_km = data['forecast']['forecastday'][0]['day']['avgvis_km']
+                avgvis_miles = data['forecast']['forecastday'][0]['day']['avgvis_miles']
+                avghumidity = data['forecast']['forecastday'][0]['day']['avghumidity']
+                weather[date] = (maxtemp_c, maxtemp_f, mintemp_c, mintemp_f, maxwind_mph, maxwind_kph,
+                                 totalprecip_mm, totalprecip_in, avgvis_km, avgvis_miles, avghumidity)
+        return weather
+
+
+api_key = "112aff8f7d04491cb93204822231906"
+location = "Wroclaw"
+date = []
+year = 2023
+month = 7
+for day in range(1, 5, 1):
+    date.append(f'{year}-{month}-{day}')
+weather_forecast = WeatherForecast(api_key)
+weather_forecast.get_forecast_month(location, date)
+data = {}
+data['13'] = (1, 2, 3, 4)
+data['3'] = ("2", 2, 3, 4)
+print(list(data.values()))
